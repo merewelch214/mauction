c49e9a9bbef629596c738224df84467bdcf3fec9
Meredith Welch <merewelch@gmail.com>
2021-11-29 21:15:07 -0500

    add header and remove unused css

---
+++ src/app/header-container/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <h1>mauction</h1>
+      <button>Buy</button>
+      <button>Sell</button>
+      <button>About Us</button>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Header;
